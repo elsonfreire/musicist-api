3ea076780c9b9f56285ce945eceb65e716d63302
elsonfreire <elsonfreirefilho@gmail.com>
2026-04-02 09:43:17 -0300

    Update User entity with column annotations

--- src/main/java/br/com/music_streak/entity/User.java
+++ src/main/java/br/com/music_streak/entity/User.java
@@ -1,5 +1,6 @@
 package br.com.music_streak.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +20,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(unique=true, nullable=false)
     private String username;
+
+    @Column(unique=true, nullable=false)
     private String email;
-    private String password_hash;
+
+    @Column(nullable=false)
+    private String passwordHash;
 }
