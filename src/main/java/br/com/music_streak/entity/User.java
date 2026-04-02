4c30fd4ecf38c0c6bdc6046b295194f4b431d171
elsonfreire <elsonfreirefilho@gmail.com>
2026-04-02 09:37:30 -0300

    Add flyway dependency and user table migration

--- src/main/java/br/com/music_streak/entity/User.java
+++ src/main/java/br/com/music_streak/entity/User.java
@@ -13,12 +13,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users", schema="public")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
+    private String username;
     private String email;
-    private String password;
+    private String password_hash;
 }
