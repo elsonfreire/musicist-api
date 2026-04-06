138506c96acb98a0ee12f32cf677b5899861f0ca
elsonfreire <elsonfreirefilho@gmail.com>
2026-04-05 22:07:19 -0300

    Remove unused dependencies

--- src/main/java/br/com/music_streak/infra/security/SecurityFilter.java
+++ src/main/java/br/com/music_streak/infra/security/SecurityFilter.java
@@ -2,12 +2,10 @@ package br.com.music_streak.infra.security;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
