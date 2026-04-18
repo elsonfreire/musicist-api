4205d1e4d5474a0e004c512e36005e988a73bb28
elsonfreire <elsonfreirefilho@gmail.com>
2026-04-18 12:19:27 -0300

    Create Practice Session exceptions

--- src/main/java/br/com/musicist/common/exceptions/ApiException.java
+++ src/main/java/br/com/musicist/common/exceptions/ApiException.java
@@ -2,7 +2,7 @@ package br.com.musicist.common.exceptions;
 
 import org.springframework.http.HttpStatus;
 
-public class ApiException extends RuntimeException{
+public class ApiException extends RuntimeException {
     private final HttpStatus status;
 
     public ApiException(String message, HttpStatus status) {
