f63630362639e31c1642384f567b39f44c525d68
felipemarley <felipemarley34@gmail.com>
2026-04-21 20:15:11 -0300

    feat: logica para manipulacao de interesses do usuario

---
+++ src/main/java/br/com/musicist/common/enums/MusicGenre.java
@@ -0,0 +1,6 @@
+package br.com.musicist.common.enums;
+
+public enum MusicGenre {
+    ROCK, POP, METAL, JAZZ, CLASSICAL, BLUES, ELECTRONIC, 
+    HIP_HOP, RNB, COUNTRY, REGGAE, FUNK, INDIE, OTHER
+}
